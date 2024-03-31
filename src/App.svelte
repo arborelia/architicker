4f0289395def84c8f2a3fe036b1b888222b2666b
Elia Robyn Lake <gh@arborelia.net>
2024-03-31 16:30:10 -0400

    editable priority vs. normal items

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,41 @@
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
   import Counter from "./lib/Counter.svelte";
+
+  let priorityTextBox: string = "Item 1";
+  let priorityTextSaved: string = priorityTextBox;
+  $: priorityItems = priorityTextSaved.split("\n");
+
+  let normalTextBox: string = "Item 2";
+  let normalTextSaved: string = normalTextBox;
+  $: normalItems = normalTextSaved.split("\n");
+
+  function updatePriorityItems() {
+    priorityTextSaved = priorityTextBox;
+  }
+
+  function updateNormalItems() {
+    normalTextSaved = normalTextBox;
+  }
 </script>
 
-<main>
-  <div class="right-column">
-    <div class="sidebar-item">sidebar item</div>
-  </div>
-</main>
+<div class="editor" style="flex: 1">
+  <h2>Priority items</h2>
+  <textarea bind:value={priorityTextBox} />
+  <button style="padding: 5px;" on:click={updatePriorityItems}>Update</button>
+
+  <h2>Normal items</h2>
+  <textarea bind:value={normalTextBox} />
+  <button style="padding: 5px;" on:click={updateNormalItems}>Update</button>
+</div>
+<div class="right-column">
+  {#each priorityItems as item}
+    <div class="sidebar-item priority">{item}</div>
+  {/each}
+  {#each normalItems as item}
+    <div class="sidebar-item">{item}</div>
+  {/each}
+</div>
 
 <style>
 </style>

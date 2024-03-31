f4146785953449610100122afe40a4a77b9ef7a5
Elia Robyn Lake <gh@arborelia.net>
2024-03-31 14:49:45 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="right-column">
+    <div class="sidebar-item">sidebar item</div>
+  </div>
+</main>
+
+<style>
+</style>
